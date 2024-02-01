c37fb46e2afacfcd54c4e645872a6cf3842f8dc4
bittere <109094899+bittere@users.noreply.github.com>
2024-02-01 12:10:37 +0530

    defaultloras

--- index.ts
+++ index.ts
@@ -19,7 +19,32 @@ if (!(await db.get(["data"])).value) {
 	);
 	await db.set(
 		["data", "loras"],
-		[{ name: "test", url: "test", on: [1, 2, 3, 5], rating: 0 }],
+		[
+{'name':'SchoolDress.safetensors','url':'https://civitai.com/api/download/models/301533?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'FloralDress.safetensors','url':'https://civitai.com/api/download/models/286157?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'PokingNipples.safetensors','url':'https://civitai.com/api/download/models/203711?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'VelvetCropTop.safetensors','url':'https://civitai.com/api/download/models/279189?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'BlackPajamasDress.safetensors','url':'https://civitai.com/api/download/models/279230?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'BlackLeatherShorts.safetensors','url':'https://civitai.com/api/download/models/279208?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'SeeThrough_Nightgown.safetensors','url':'https://civitai.com/api/download/models/252290?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'SweaterPencilSkirt.safetensors','url':'https://civitai.com/api/download/models/239644?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'Beauty.safetensors','url':'https://civitai.com/api/download/models/211209?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'FaceLove.safetensors','url':'https://civitai.com/api/download/models/92758','on':[3],'rating':0},
+{'name':'Windowsill.safetensors','url':'https://civitai.com/api/download/models/193789','on':[3],'rating':0},
+{'name':'GirlLivingAlone.safetensors','url':'https://civitai.com/api/download/models/264865?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'ForestLight.safetensors','url':'https://civitai.com/api/download/models/127699?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'Trees.safetensors','url':'https://civitai.com/api/download/models/307012','on':[3],'rating':0},
+{'name':'EtherealChiffon.safetensors','url':'https://civitai.com/api/download/models/163075','on':[3],'rating':0},
+{'name':'AsianCasual.safetensors','url':'https://civitai.com/api/download/models/71767','on':[3],'rating':0},
+{'name':'DatingAttireV6.safetensors','url':'https://civitai.com/api/download/models/228470?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'DatingAttireV5.safetensors','url':'https://civitai.com/api/download/models/207675?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'DatingAttireV4.safetensors','url':'https://civitai.com/api/download/models/189285?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'DatingAttireV3.safetensors','url':'https://civitai.com/api/download/models/181812?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'DatingAttireV2.safetensors','url':'https://civitai.com/api/download/models/159298?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'DatingAttireV1.safetensors','url':'https://civitai.com/api/download/models/156890?type=Model&format=SafeTensor','on':[3],'rating':0},
+{'name':'Tennis.safetensors','url':'https://civitai.com/api/download/models/65659','on':[3],'rating':0},
+{'name':'SDXLAsianBeauties.safetensors','url':'https://civitai.com/api/download/models/136869?type=Model&format=SafeTensor','on':[3],'rating':0},
+]
 	);
 	await db.set(
 		["data", "presetCommands"],
