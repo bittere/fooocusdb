c177aaf67bd8231b9172882f3a02267e2a7ae425
bittere <109094899+bittere@users.noreply.github.com>
2024-02-01 17:15:23 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const db = await Deno.openKv();
 if (!(await db.get(["data"])).value) {
 	await db.set(
 		["data", "models"],
-		[{"name":"RMSDXLOrion","url":"https://civitai.com/api/download/models/288024","on":[],"rating":0},{"name":"PicXReal","url":"https://civitai.com/api/download/models/272376","on":[],"rating":0}]
+		[{"name":"RMSDXLOrion","url":"https://civitai.com/api/download/models/288024","on":[],"rating":0},{"name":"MagMix","url":"https://civitai.com/api/download/models/306220","on":[],"rating":0}]
 	);
 	await db.set(
 		["data", "loras"],
@@ -45,8 +45,7 @@ if (!(await db.get(["data"])).value) {
 	);
 	await db.set(
 		["data", "presetCommands"],
-		[{ name: "webui", desc: `cd /home/studio-lab-user/sagemaker-studiolab-notebooks/Fooocus; sed "s/minimum=0.1, maximum=1.0/minimum=0.0, maximum=1.0/" webui.py -i`, on: [3] }, {name:"preset", desc: `cd /home/studio-lab-user/sagemaker-studiolab-notebooks/Fooocus/presets; sed "s#https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_v8Rundiffusion.safetensors#https://civitai.com/api/download/models/288024?token=9b4b80f2f77161b8636ace0a06f35a61#" default.json -i
-!sed "s#juggernautXL_v8Rundiffusion.safetensors#RMSDXLOrion.safetensors#" default.json -i`, on: [3] }],
+		[{ name: "webui", desc: `cd /home/studio-lab-user/sagemaker-studiolab-notebooks/Fooocus; sed "s/minimum=0.1, maximum=1.0/minimum=0.0, maximum=1.0/" webui.py -i`, on: [3] }, {name:"preset", desc: `cd /home/studio-lab-user/sagemaker-studiolab-notebooks/Fooocus/presets; sed "s#https://huggingface.co/lllyasviel/fav_models/resolve/main/fav/juggernautXL_v8Rundiffusion.safetensors#https://civitai.com/api/download/models/288024?token=9b4b80f2f77161b8636ace0a06f35a61#" default.json -i; sed "s#juggernautXL_v8Rundiffusion.safetensors#RMSDXLOrion.safetensors#" default.json -i`, on: [3] }],
 	);
 
 	await db.set(["data", "ngrok"], "2biGLq0E2b1dNYNP33kN0IjYYZh_7sV8HTNKuQHnmPnJ3di9K");
